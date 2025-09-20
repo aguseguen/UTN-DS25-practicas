5cf7fda626f47f3f13c9b2f47a092aa2fca2df33
aguseguen <agustinaeguen3@gmail.com>
2025-09-20 11:35:19 -0300

    Subo avances

--- React/libreria-react/src/App.tsx
+++ React/libreria-react/src/App.tsx
@@ -1,21 +1,21 @@
-import Header from './components/Header';
+// En tu archivo App.tsx
+import './App.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom'; 
-import '../src/App.css'
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
-      <Nav /> 
-
-      <main className="container">
-        <Outlet /> 
+      <Nav />
+      <main className="main-content">
+        {/* Aquí es donde se renderizan tus páginas, como HomePage */}
+        <HomePage />
       </main>
-      
       <Footer />
-    </>
+    </div>
   );
 }
 
